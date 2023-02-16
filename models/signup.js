8ec4af7cb4d09a28b8d6d664a124d85b6c3231c5
satvik-vm <satvik.verma@iiitb.ac.in>
2023-02-17 01:22:47 +0530

    reverting the revert

--- models/signup.js
+++ models/signup.js
@@ -23,6 +23,11 @@ const signupSchema = new mongoose.Schema({
         // },
     },
 
+    email: {
+        type: String,
+        required: [true],
+    },
+
     password: {
         type : String,
         required: [true]
