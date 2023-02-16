ee044261d538b12637218d62493e2eeab189c788
satvik-vm <satvik.verma@iiitb.ac.in>
2023-02-17 01:30:32 +0530

    Front End

---
+++ matx-react-master/src/app/views/dashboard/shared/UpgradeCard.jsx
@@ -0,0 +1,58 @@
+import { Button, Card, styled } from '@mui/material';
+import { convertHexToRGB } from 'app/utils/utils';
+
+const CardRoot = styled(Card)(({ theme }) => ({
+  marginBottom: '24px',
+  padding: '24px !important',
+  [theme.breakpoints.down('sm')]: { paddingLeft: '16px !important' },
+}));
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  boxShadow: 'none',
+  textAlign: 'center',
+  position: 'relative',
+  padding: '24px !important',
+  background: `rgb(${convertHexToRGB(theme.palette.primary.main)}, 0.15) !important`,
+  [theme.breakpoints.down('sm')]: { padding: '16px !important' },
+}));
+
+const Paragraph = styled('p')(({ theme }) => ({
+  margin: 0,
+  paddingTop: '24px',
+  paddingBottom: '24px',
+  color: theme.palette.text.secondary,
+}));
+
+const UpgradeCard = () => {
+  return (
+    <CardRoot>
+      <StyledCard elevation={0}>
+        <Paragraph>
+          What type of <b>Transaction</b>  <br /> would you like?  
+        </Paragraph>
+        
+
+        <Button
+          size="large"
+          color="primary"
+          variant="contained"
+          sx={{ textTransform: 'uppercase' }}
+        >
+          Deposit
+        </Button>
+        <br/>
+        <br/>
+        <Button
+          size="large"
+          color="primary"
+          variant="contained"
+          sx={{ textTransform: 'uppercase' }}
+        >
+          Withdrawal
+        </Button>
+      </StyledCard>
+    </CardRoot>
+  );
+};
+
+export default UpgradeCard;
